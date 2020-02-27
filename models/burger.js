db7970f0f30c51e93755be0e867bd9d4992c8e98
Shannon Scirghio <shanscirg7@gmail.com>
2020-02-27 00:35:50 -0700

    created the rest of the files

--- models/burger.js
+++ models/burger.js
@@ -1,19 +1,18 @@
 const orm = require("../config/orm.js");
 
 const burger = {
-    all: function(cb) {
-      orm.all("cats", function(res) {
+    selectAll: function(cb) {
+      orm.selectAll("burgers", function(res) {
         cb(res);
       });
     },
-    // The variables cols and vals are arrays.
-    create: function(cols, vals, cb) {
-      orm.create("cats", cols, vals, function(res) {
+    insertOne: function(cols, vals, cb) {
+      orm.insertOne("burgers", cols, vals, function(res) {
         cb(res);
       });
     },
-    update: function(objColVals, condition, cb) {
-      orm.update("cats", objColVals, condition, function(res) {
+    updateOne: function(objColVals, condition, cb) {
+      orm.updateOne("burgers", objColVals, condition, function(res) {
         cb(res);
       });
     }
